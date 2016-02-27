a030764f93a35ccd81365592898d039e02278216
KAREN KONG <kkong006@ucr.edu>
2016-02-26 23:31:35 -0800

    Added project files.

---
+++ app/src/main/java/com/eje_c/rajawalicardboard/renderImage.java
@@ -0,0 +1,39 @@
+package com.eje_c.rajawalicardboard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+
+import org.rajawali3d.cardboard.RajawaliCardboardRenderer;
+import org.rajawali3d.cardboard.RajawaliCardboardView;
+public class renderImage extends CardboardActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RajawaliCardboardView view = new RajawaliCardboardView(this);
+        setContentView(view);
+        setCardboardView(view);
+
+        RajawaliCardboardRenderer renderer = new MyRenderer(this);
+        view.setRenderer(renderer);
+        view.setSurfaceRenderer(renderer);
+    }
+    // private static class MyRenderer extends RajawaliCardboardRenderer {
+//        public MyRenderer(Context context) {
+//            super(context);
+//        }
+//
+//        @Override
+//        protected void initScene() {
+//
+//            // create your scene
+//
+//        }
+//    }
+}
+
+
