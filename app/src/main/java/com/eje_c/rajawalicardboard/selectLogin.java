267722d5ceb09854a5130abaf6218ace25754c1e
KAREN KONG <kkong006@ucr.edu>
2016-02-26 23:58:41 -0800

    Change background color.

--- app/src/main/java/com/eje_c/rajawalicardboard/selectLogin.java
+++ app/src/main/java/com/eje_c/rajawalicardboard/selectLogin.java
@@ -1,7 +1,7 @@
 package com.eje_c.rajawalicardboard;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 
 public class selectLogin extends Activity {
 
@@ -10,4 +10,5 @@ public class selectLogin extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_login);
     }
+
 }
