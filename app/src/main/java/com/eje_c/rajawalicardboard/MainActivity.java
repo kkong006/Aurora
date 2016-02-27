7df1685459d2b501324792df2af8ef16b502748e
KAREN KONG <kkong006@ucr.edu>
2016-02-27 01:13:07 -0800

    Rename main.

--- app/src/main/java/com/eje_c/rajawalicardboard/MainActivity.java
+++ app/src/main/java/com/eje_c/rajawalicardboard/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
     }
 
 }
